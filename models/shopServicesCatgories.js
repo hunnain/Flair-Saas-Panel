b37c48f09f8f2ee414b1c31d497f11e4c45c8515
Hunnain Pasha <hunnainpashahgchgc@gmail.com>
2023-02-09 20:44:12 +0500

    changes

---
+++ models/shopServicesCatgories.js
@@ -0,0 +1,34 @@
+const mongoose  = require('mongoose');
+const bcrypt    = require('bcrypt');
+const Schema    = mongoose.Schema;
+const crypto = require('crypto');
+
+const ShopServicesCategorySchema = new Schema({
+    shopAdminAccountId:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'shopsadminaccount',
+    },
+    category: {
+        type: String,
+    },
+    workingLocation: [{ type : mongoose.Schema.Types.ObjectId, ref: 'shopbranches' }],
+    shopServicesAttachWithThisCategory: [{ type : mongoose.Schema.Types.ObjectId, ref: 'shopservices' }],
+    created_at      : { type: Date, default: Date.now },
+})
+
+// UserSchema.methods.isValidPassword = async function(password) {
+//     const user      = this;
+//     const compare   = await bcrypt.compare(password, user.password);
+//     return compare;
+// }
+
+// Forget Passord Method
+// UserSchema.methods.generatePasswordReset = function() {
+//     this.resetPasswordToken = Math.floor(100000 + Math.random() * 900000);
+//     this.resetPasswordExpires = Date.now() + 3600000; //expires in an hour
+// };
+
+const ShopServicesCategoryModel = mongoose.model('shopservicescategories', ShopServicesCategorySchema);
+
+module.exports  = ShopServicesCategoryModel;
